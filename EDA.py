0a02b7c48660fb8f2131252063823ff23118a8c6
Yongjae Cho <whdydwo0431@naver.com>
2026-03-19 11:48:52 +0900

    cloned openvla

--- EDA.py
+++ EDA.py
@@ -6,9 +6,6 @@ builder = tfds.builder_from_directory('D:/bridge_data/bridge_orig/1.0.0')
 data = builder.as_dataset(split='train')
 print(builder.info)
 
-# 데이터 로드
-data = builder.as_dataset(split='train')
-
 # 에피소드 1개 확인
 for episode in data.take(1):
     steps = list(episode['steps'])
