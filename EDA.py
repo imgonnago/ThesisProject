90b9627761885f7a4205e6385610789ec209a5c2
Yongjae Cho <whdydwo0431@naver.com>
2026-03-23 12:07:17 +0900

    f

--- EDA.py
+++ EDA.py
@@ -1,17 +1,25 @@
 import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
 import numpy as np
+import platform
 
-builder = tfds.builder_from_directory('D:/bridge_data/bridge_orig/1.0.0')
-data = builder.as_dataset(split='train')
-print(builder.info)
+def data_info():
+    if platform.system() == 'Window':
+        builder = tfds.builder_from_directory('D:/bridge_data/bridge_orig/1.0.0')
+        data = builder.as_dataset(split='train')
+        print(builder.info)
+        return data
+    elif platform.system() == 'Darwin':
+        builder = tfds.builder_from_directory('/Volumes/YONGJAE/bridge_data/bridge_orig/1.0.0')
+        data = builder.as_dataset(split='train')
+        print(builder.info)
+        return data
 
-# 에피소드 1개 확인
+data = data_info()
 for episode in data.take(1):
     steps = list(episode['steps'])
     print("에피소드 길이:", len(steps))
 
-    # 첫 번째 스텝 확인
     step = steps[0]
     print("언어 지시문:", step['language_instruction'].numpy().decode('utf-8'))
     print("action:", step['action'].numpy())
